b72c0e49aae0da653d94828d3e13f019ecb30218
Zhamila1994 <154753736+Zhamila1994@users.noreply.github.com>
2024-04-22 16:17:42 +0500

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,22 @@
+package kz.bitlab.techorda.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Operators {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String department;
+}
